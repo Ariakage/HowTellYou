15854bc0d43c1553f930b7e1cbb38a73a9c073a7
chenjj100419 <2026159790@qq.com>
2023-07-16 12:27:15 +0800

    添加修改desc

--- src/main.go
+++ src/main.go
@@ -694,6 +694,70 @@ func main() {
 						return
 					}
 				case "description":
+					tk := respone_content["token"]
+					desc := respone_content["description"]
+					if tk == nil || desc == nil {
+						var d map[string]interface{} = make(map[string]interface{})
+						d["status"] = "failed"
+						d["reason"] = "token or gender is null"
+						var m *map[string]interface{} = makeResponse(0, d)
+						m_b, err := json.Marshal(m)
+						if err != nil {
+							fmt.Println(err)
+						}
+						ctx.Text(string(m_b))
+						return
+					}
+					t, err := SCDecryptString(tk.(string), se_key, "aes")
+					if err != nil {
+						fmt.Println(err)
+					}
+					r, _ := base64.StdEncoding.DecodeString(t)
+					uid := strings.Split(string(r), "@")[0]
+					id, err := strconv.Atoi(uid)
+					if err != nil {
+						fmt.Println(err)
+					}
+					if _, ok := globalTokenMap[id]; ok {
+						for e := globalTokenMap[id].Front(); e != nil; e = e.Next() {
+							if e.Value.(map[string]string)["token"] == tk {
+								_, err := db.Exec("UPDATE hty_user SET `description`=? WHERE `id` = ?", desc.(string), id)
+								if err != nil {
+									fmt.Println(err)
+								}
+								var d map[string]interface{} = make(map[string]interface{})
+								d["status"] = "success"
+								var m *map[string]interface{} = makeResponse(200, d)
+								m_b, err := json.Marshal(m)
+								if err != nil {
+									fmt.Println(err)
+								}
+								ctx.Text(string(m_b))
+								return
+							}
+						}
+						var d map[string]interface{} = make(map[string]interface{})
+						d["status"] = "failed"
+						d["reason"] = "token is invalid"
+						var m *map[string]interface{} = makeResponse(0, d)
+						m_b, err := json.Marshal(m)
+						if err != nil {
+							fmt.Println(err)
+						}
+						ctx.Text(string(m_b))
+						return
+					} else {
+						var d map[string]interface{} = make(map[string]interface{})
+						d["status"] = "failed"
+						d["reason"] = "token is invalid"
+						var m *map[string]interface{} = makeResponse(0, d)
+						m_b, err := json.Marshal(m)
+						if err != nil {
+							fmt.Println(err)
+						}
+						ctx.Text(string(m_b))
+						return
+					}
 				case "pwd":
 				default:
 					var d map[string]interface{} = make(map[string]interface{})
